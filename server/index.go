33f7fd53a070e5e5ed7c00b6d58e3a63ec34a8de
Tarik Kilic <tarikkilic07@gmail.com>
2022-01-17 20:37:31 +0300

    Define route with naming the function

--- server/index.go
+++ server/index.go
@@ -6,15 +6,17 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+func helloWorld(c *gin.Context) {
+
+	c.String(http.StatusOK, "hello world")
+
+}
+
 func main() {
 
 	r := gin.Default()
 
-	r.GET("/", func(c *gin.Context) {
-
-		c.String(http.StatusOK, "hello world")
-
-	})
+	r.GET("/", helloWorld)
 
 	r.Run(":3000")
 
